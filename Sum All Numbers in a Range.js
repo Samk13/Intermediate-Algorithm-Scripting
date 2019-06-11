11333c1f88cfec7730f7b105c96ca7bc00acb328
sam_arbid <samarbid13@users.noreply.github.com>
2019-06-11 11:46:07 +0200

    base

---
+++ Sum All Numbers in a Range.js
@@ -0,0 +1,17 @@
+
+// Intermediate Algorithm Scripting: Sum All Numbers in a Range
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them.
+
+// The lowest number will not always come first.
+
+
+function sumAll(arr) {
+    return 1;
+  }
+  
+
+  
+  console.log( 
+  sumAll([1, 4])
+  );
+
