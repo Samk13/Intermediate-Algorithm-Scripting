b4faca841952036872164fd70acad49c2caa89b8
sam_arbid <samarbid13@users.noreply.github.com>
2019-06-12 13:58:55 +0200

    Update challenges

--- Sorted Union.js
+++ Sorted Union.js
@@ -8,23 +8,56 @@
 // Check the assertion tests for examples.
 
 
-function uniteUnique(arr) {
-    return arr;
+// function uniteUnique(arr) {
+//     let placeHolder = arr;
+//     for(let i = 0; i< arguments.length; i++){
+//         for (let j=0 ; j< arguments[i].length;j++){
+//             if(placeHolder.indexOf(arguments[i][j]) === -1){
+//                 placeHolder.push(arguments[i][j]);
+//             }
+//         }
+//     }
 
-    
+
+//     arr1 = placeHolder;
+//     return arr1;
+//   }
+  
+
+//   console.log(
+  
+//  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])
+
+// //   uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) //should return [1, 3, 2, 5, 4].
+// //   uniteUnique([1, 3, 2], [1, [5]], [2, [4]])// should return [1, 3, 2, [5], [4]].
+// //   uniteUnique([1, 2, 3], [5, 2, 1]) //should return [1, 2, 3, 5].
+// //   uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) //should return [1, 2, 3, 5, 4, 6, 7, 8].
+
+
+
+//   );
+
+
+
+
+
+function uniteUnique(arr) {
+    const args = [].concat(...arguments);
+    return [...new Set(args)];
+    return arr;
   }
+  console.log(...arguments);
   
 
   console.log(
   
-  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])
+ //uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])
 
 //   uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) //should return [1, 3, 2, 5, 4].
 //   uniteUnique([1, 3, 2], [1, [5]], [2, [4]])// should return [1, 3, 2, [5], [4]].
-//   uniteUnique([1, 2, 3], [5, 2, 1]) //should return [1, 2, 3, 5].
+   uniteUnique([1, 2, 3], [5, 2, 1]) //should return [1, 2, 3, 5].
 //   uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) //should return [1, 2, 3, 5, 4, 6, 7, 8].
 
 
 
   );
-
